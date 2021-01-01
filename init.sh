296f66180d05b7314fbdc705aea497d9c5c9d453
Daniel Medrano Huerta <daniel@quicktortoise.com>
2021-01-01 13:10:07 -0600

    Cambiar DotNET SDK 3.1 a 5.0

--- init.sh
+++ init.sh
@@ -44,7 +44,7 @@ curl -sS https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 # Instalar software faltante
-sudo apt install -y dotnet-sdk-3.1 spotify-client nodejs yarn sublime-text meld terminator firefox firefox-locale-es geary
+sudo apt install -y dotnet-sdk-5.0 spotify-client nodejs yarn sublime-text meld terminator firefox firefox-locale-es geary
 
 # Maicra
 sudo dpkg -i https://launcher.mojang.com/download/Minecraft.deb
