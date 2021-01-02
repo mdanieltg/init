b807177727f63bf7ce6c11365104a13d3937f36c
Daniel Medrano Huerta <daniel@quicktortoise.com>
2021-01-02 10:45:51 -0600

    Cambiar versión de Node.js a 14

--- init.sh
+++ init.sh
@@ -38,7 +38,7 @@ curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key 
 curl -sS https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 
 # El buen Node.js
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 # Instalar software faltante
 sudo apt install -y dotnet-sdk-5.0 spotify-client nodejs yarn sublime-text meld terminator firefox firefox-locale-es geary
