dc6e9504be05f3a9a65f1b9a8a18343810a0db15
Daniel Medrano Huerta <daniel@quicktortoise.com>
2021-07-30 11:37:14 -0500

    Optimizar mensajes

--- fonts.sh
+++ fonts.sh
@@ -7,14 +7,16 @@ F='\033[0m'
 
 if [ $UID -eq 0 ]; then
 	FONTS="/usr/share/fonts"
-	echo "Instalando fuentes a nivel global (sistema)."
+	MSG="global (sistema)"
 else
-	echo "Instalando fuentes a nivel local (usuario)."
+	MSG="local (usuario)"
 fi
 
-echo -e "\n${D}-> ${O}Instalando fuentes${F}"
+echo -e "\033[0;33m--|${O} Instalando fuentes a nivel $MSG ${F}\033[0;33m|--\n"
+
 
 # MesloLGS NF
+echo -e "\n${D}-> ${O}MesloLGS NF${F}"
 MLN="$FONTS/meslolgs-nf"
 mkdir -p "$MLN"
 wget -nv -O "$MLN/MesloLGS NF Regular.ttf" "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf"
@@ -23,12 +25,14 @@ wget -nv -O "$MLN/MesloLGS NF Italic.ttf" "https://github.com/romkatv/powerlevel
 wget -nv -O "$MLN/MesloLGS NF Bold Italic.ttf" "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf"
 
 # Hack
+echo -e "\n${D}-> ${O}Hack${F}"
 wget -nv -O /tmp/hack.tar.xz 'https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.tar.xz'
 mkdir -p "$FONTS/hack"
 tar -xf /tmp/hack.tar.xz -C "$FONTS/hack"
 rm /tmp/hack.tar.xz
 
 # JetBrains Mono
+echo -e "\n${D}-> ${O}JetBrains Mono${F}"
 wget -nv -O /tmp/jb.zip 'https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip'
 unzip -q /tmp/jb.zip -d /tmp/jb
 mv /tmp/jb/fonts/ttf "$FONTS/jetbrains-mono"
